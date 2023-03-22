92fc7127c0b5cd48786500b4b5a00b240c94bc4d
paras9sid <sidharthjainp9@gmail.com>
2023-03-22 17:05:38 +0530

    Redirecting requests

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" value="submit"</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button>Submit</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy text in txt file - content"); //will create file in the same folder
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
