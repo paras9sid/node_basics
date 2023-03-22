cd8cb2344659725c9a4592d8378b10ac83e67316
paras9sid <sidharthjainp9@gmail.com>
2023-03-22 16:49:43 +0530

    Req&Res headers & Routing req's

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" value="submit"</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from node.js server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
---
+++ index.js
@@ -0,0 +1,21 @@
+// // function hello() {
+// //   return "hello";
+// // }
+// // console.log(hello());
+
+// // learning Node
+
+// // const mission = "learn";
+// // const mission = "not learn";
+
+// // using process argv method
+// const mission = process.argv[2]; // 2nd argument - node -0th argument - index.js - 1st argument - learn or any string 2nd argument in terminal
+
+// if (mission === "learn") {
+//   console.log("time to write the code");
+// } else {
+//   //   console.log("is " + mission + "really fun");
+
+//   // using ECMASCRIPT LATEST-wuth backticks -interpolition method
+//   console.log(`is ${mission} really fun`);
+// }
