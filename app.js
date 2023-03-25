029c3f2e85fb8c9a5bd9724cca0171a438a423cb
paras9sid <sidharthjainp9@gmail.com>
2023-03-25 18:18:07 +0530

    body-parser used url encoded

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 //package imports
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+//registering the parser - add top before all the middlewares route
+// extended false objetc remove warning - body-parser deprecated undefined extended: provide extended option app.js:8:20
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //use allows to add us new middleware function
 //route added with app.use - handling routes
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
+  console.log("In product middleware");
   // sending response
-  res.send("<h1>Add product page</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
 });
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body); //gives undefined ans in console
+  res.redirect("/");
+});
+
+//home route / should be at the end so that other routes can be visited otherwise only home will visit again
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
+  console.log("This always run home route");
   // sending response
   res.send("<h1>Response from the server!</h1>");
 });
