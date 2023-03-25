9e7f555eab453607c9d221a71652fcc64974c433
paras9sid <sidharthjainp9@gmail.com>
2023-03-25 19:13:42 +0530

    404 error page addded for all unknown routes

--- app.js
+++ app.js
@@ -15,4 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+//adding 404 error page for any route we cont have written
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000); //shortcut for server removing above 2 lines
