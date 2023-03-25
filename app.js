cef6b04eb7a09c59eade64a990ea1a98df7c8e1c
paras9sid <sidharthjainp9@gmail.com>
2023-03-25 12:39:18 +0530

    handling differet routes via middleware

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 //package imports
-// const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
 //use allows to add us new middleware function
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); // allows the request to continue to next middleware in line
-});
-app.use((req, res, next) => {
+//route added with app.use - handling routes
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
-
+  // sending response
+  res.send("<h1>Add product page</h1>");
+});
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
   // sending response
   res.send("<h1>Response from the server!</h1>");
 });
-
-// const server = http.createServer(app); // as handler has requesthandler funciton assigned to it.
-
-// server.listen(3000);
 
 app.listen(3000); //shortcut for server removing above 2 lines
