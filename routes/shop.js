cdaca5868f5bbe0ed752a7fc076d4857a55f9c2c
paras9sid <sidharthjainp9@gmail.com>
2023-03-26 18:55:07 +0530

    Adding CSS styling

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,6 @@ const rootDir = require("../util/path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // res.sendFile("/views/shop.html"); or   res.sendFile("./views/shop.html"); will not work
-  //method to add path
   // __dirname points to path in the same foler t, so to go up add one more  '../' like this to go up on level
   res.sendFile(path.join(rootDir, "views", "shop.html")); //dont add / in path anywhere as join is used
 });
