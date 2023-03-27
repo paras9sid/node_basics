7ee0567e8a4aaf470f50238f80b9e421fbbaac93
paras9sid <sidharthjainp9@gmail.com>
2023-03-27 18:40:38 +0530

    CSS & .pug file fixed

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-//file imports
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 const app = express();
 
 //setting global config values
 app.set("view engine", "pug");
 app.set("views", "views");
+
+//file imports
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 //registering the parser - add top before all the middlewares route
 // extended false objetc remove warning - body-parser deprecated undefined extended: provide extended option app.js:8:20
