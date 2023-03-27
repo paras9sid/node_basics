5a17d6b84ea1b32261e9a5912ecd63892f4ee9b7
paras9sid <sidharthjainp9@gmail.com>
2023-03-27 16:28:29 +0530

    Outputting dynamic content-1

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,8 @@ const adminRoutes = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // __dirname points to path in the same foler t, so to go up add one more  '../' like this to go up on level
-  // console.log("shop js", adminRoutes.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html")); //dont add / in path anywhere as join is used
-  res.render("shop");
+  const products = adminRoutes.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
