2f70995bad50f08782e17d893a7baced6591cda0
paras9sid <sidharthjainp9@gmail.com>
2023-03-30 12:01:48 +0530

    Product-store,display,edit,delete set

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,12 +10,6 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect("/");
-};
-
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
@@ -24,4 +18,14 @@ exports.getProducts = (req, res, next) => {
       path: "/admin/products",
     });
   });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, price, description);
+  product.save();
+  res.redirect("/");
 };
