c310021aefe703eae9cd1257f9e0b450ef2e73f3
paras9sid <sidharthjainp9@gmail.com>
2023-03-30 20:46:13 +0530

    query params used - data prepopulated,edit,editpage linked

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
+    editing: false,
   });
 };
 
@@ -28,4 +26,36 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  //checking query parameters/params
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  } //fetching product first/product info
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -15,5 +15,9 @@ router.get("/products", adminController.getProducts);
 //methods are different paths are same so it differs
 router.post("/add-product", adminController.postAddProduct);
 
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product");
+
 // module.exports = router;
 module.exports = router;
